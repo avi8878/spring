2b1f7043c8b3937a190434b814931f73cdf6db89
Avinash <mr.avinashsingh@gmail.com>
2025-06-15 21:11:05 +0530

    added test cases.

--- src/test/java/com/avi/batch/BatchApplicationTests.java
+++ src/test/java/com/avi/batch/BatchApplicationTests.java
@@ -2,12 +2,16 @@ package com.avi.batch;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.Assert.assertEquals;
 
 @SpringBootTest
 class BatchApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void testSayHello() {
+		String result = "Avinash";
+		assertEquals("Avinash", result);
 	}
-
 }
+
+
