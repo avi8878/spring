25d1489f9bca74489b2276dcdf43916fe7905820
Avinash <mr.avinashsingh@gmail.com>
2025-06-15 21:20:10 +0530

    logger added

--- src/test/java/com/avi/batch/BatchApplicationTests.java
+++ src/test/java/com/avi/batch/BatchApplicationTests.java
@@ -1,16 +1,20 @@
 package com.avi.batch;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.Assert.assertEquals;
 
 @SpringBootTest
+@Slf4j
 class BatchApplicationTests {
 
 	@Test
 	void testSayHello() {
-		String result = "Avinash";
-		assertEquals("Avinash", result);
+      String result = "Avinash";
+      log.info("Testing ended sayHello method");
+	  assertEquals("Avinash", result);
+	  log.info("Testing ended sayHello method");
 	}
 }
 
